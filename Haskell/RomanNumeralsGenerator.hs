5527f805c0b3fa7ce2e3065b4d10f338731c4eaf
Gryff <gryffskin@gmail.com>
2016-05-23 11:06:53 +0100

    Use foldl to accumulate over characters, use a toDecimal helper function

--- Haskell/RomanNumeralsGenerator.hs
+++ Haskell/RomanNumeralsGenerator.hs
@@ -1,4 +1,7 @@
 module RomanNumeralsGenerator where
 
 convert :: String -> Int 
-convert numerals = length numerals
+convert numerals = foldl (\acc x -> acc + toDecimal x) 0 numerals
+
+toDecimal :: Char -> Int 
+toDecimal n = if n == 'V' then 5 else 1
